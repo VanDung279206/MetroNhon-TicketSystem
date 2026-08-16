1a516cab8ef260d9054c616ded8a0f0590c30429
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-16 15:36:10 +0700

    thêm thuộc tính, constructor, getter và setter, sửa hàm toString

--- TicketSystem/src/model/VeLuot.java
+++ TicketSystem/src/model/VeLuot.java
@@ -1,5 +1,55 @@
 package model;
 
-public class VeLuot {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class VeLuot extends VeMetro {
     //kế thừa VeMetro
+    private Ga gaDi;
+    private Ga gaDen;
+    private LocalDate ngaySuDung;
+
+    public VeLuot(String maVe, HanhKhach hanhKhach, LocalDateTime ngayMua, double giaVe, boolean trangThai, Ga gaDi, Ga gaDen, LocalDate ngaySuDung) {
+        super(maVe, hanhKhach, ngayMua, giaVe, trangThai);
+        this.gaDi = gaDi;
+        this.gaDen = gaDen;
+        this.ngaySuDung = ngaySuDung;
+    }
+
+    public Ga getGaDi() {
+        return gaDi;
+    }
+
+    public void setGaDi(Ga gaDi) {
+        this.gaDi = gaDi;
+    }
+
+    public Ga getGaDen() {
+        return gaDen;
+    }
+
+    public void setGaDen(Ga gaDen) {
+        this.gaDen = gaDen;
+    }
+
+    public LocalDate getNgaySuDung() {
+        return ngaySuDung;
+    }
+
+    public void setNgaySuDung(LocalDate ngaySuDung) {
+        this.ngaySuDung = ngaySuDung;
+    }
+
+    @Override
+    public String getLoaiVe() {
+        return "VE_LUOT";
+    }
+
+    @Override
+    public String toString() {
+        return getMaVe() + " | vé lượt | " +
+                gaDi.getTenGa() + " -> " +
+                gaDen.getTenGa() + " | " +
+                getGiaVe() + "VND";
+    }
 }
