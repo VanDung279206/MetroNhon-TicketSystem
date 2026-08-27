712aac11134a54d9f0088abd64593aa6c71c150f
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-28 05:14:22 +0700

    thêm 1 vài hằng số

--- TicketSystem/src/utils/Constants.java
+++ TicketSystem/src/utils/Constants.java
@@ -1,12 +1,20 @@
 package utils;
 
+import java.nio.file.Path;
+
 public final class Constants {
-    //chứa các hằng số
+    private static final String DATA_DIRECTORY =
+            System.getProperty("metro.data.dir", "data");
 
-    public static final String FILE_TAI_KHOAN = "data/tai_khoan.txt";
-    public static final String FILE_HANH_KHACH = "data/hanh_khach.txt";
-    public static final String FILE_GA = "data/ga.txt";
-    public static final String FILE_VE_DA_BAN = "data/ve_da_ban.txt";
+    public static final String FILE_TAI_KHOAN =
+            Path.of(DATA_DIRECTORY, "tai_khoan.txt").toString();
+    public static final String FILE_HANH_KHACH =
+            Path.of(DATA_DIRECTORY, "hanh_khach.txt").toString();
+    public static final String FILE_GA =
+            Path.of(DATA_DIRECTORY, "ga.txt").toString();
+    public static final String FILE_VE_DA_BAN =
+            Path.of(DATA_DIRECTORY, "ve_da_ban.txt").toString();
 
-    private Constants(){} // private - không cho tạo hàm new
+    private Constants() {
+    }
 }
