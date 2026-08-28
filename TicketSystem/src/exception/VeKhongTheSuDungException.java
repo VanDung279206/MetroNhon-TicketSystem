4cb860372f0a5ebe3c9f182f009b073af0a5bbcf
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-29 01:21:24 +0700

    ngoại lệ với đăng ký vé

---
+++ TicketSystem/src/exception/VeKhongTheSuDungException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class VeKhongTheSuDungException extends IllegalStateException {
+    public VeKhongTheSuDungException(String message) {
+        super(message);
+    }
+}
